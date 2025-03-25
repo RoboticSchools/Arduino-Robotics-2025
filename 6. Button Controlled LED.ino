9668ced5410792f0e10cb7679bbb4fff929b59d4
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 16:57:16 +0530

    6. Button Controlled LED.ino

---
+++ 6. Button Controlled LED.ino
@@ -0,0 +1,25 @@
+/*
+Components Used:
+- Arduino Board
+- Sensor Shield
+- Push Button
+- LED
+- Breadboard
+- Jumper Wires
+*/
+
+const int buttonPin = 7; // Button connected to Pin 7
+const int ledPin = 8;    // LED connected to Pin 8
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP); // Set Button pin as input with internal pull-up resistor
+  pinMode(ledPin, OUTPUT);          // Set LED pin as output
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == 1) {  // Check if button is pressed (Active LOW)
+    digitalWrite(ledPin, HIGH);         // Turn on LED
+  } else {
+    digitalWrite(ledPin, LOW);          // Turn off LED
+  }
+}
