eac566fd23feb61874ffa9f4580df3a2468180cf
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 22:04:30 +0530

    Create R58. IR Remote-Controlled LED.ino

---
+++ R58. IR Remote-Controlled LED.ino
@@ -0,0 +1,43 @@
+/*
+Components used:
+- Arduino Board
+- Sensor Shield
+- IR Receiver Module
+- LED
+- Remote Control
+- Jumper Wires
+*/
+
+#include <IRremote.h>
+
+#define IR_RECEIVE_PIN 8  // IR receiver connected to Sensor Shield
+#define LED_PIN 6         // LED connected to Sensor Shield
+
+// IR remote button codes
+#define IR_BUTTON_ON 12
+#define IR_BUTTON_OFF 24
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);  // Set LED pin as output
+  IrReceiver.begin(IR_RECEIVE_PIN);  // Start IR receiver
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    int command = IrReceiver.decodedIRData.command;
+    IrReceiver.resume();  // Continue receiving IR signals
+
+    if (command == IR_BUTTON_ON) {
+      Serial.println("LED ON");
+      digitalWrite(LED_PIN, HIGH);  // Turn LED on
+    } 
+    else if (command == IR_BUTTON_OFF) {
+      Serial.println("LED OFF");
+      digitalWrite(LED_PIN, LOW);   // Turn LED off
+    } 
+    else {
+      Serial.println("Button not recognized");
+    }
+  }
+}
