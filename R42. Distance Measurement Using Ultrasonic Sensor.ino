b2f5fc686542ea09138bb43228a7d6673d77ca6c
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 21:09:25 +0530

    Create R42. Distance Measurement Using Ultrasonic Sensor.ino

---
+++ R42. Distance Measurement Using Ultrasonic Sensor.ino
@@ -0,0 +1,41 @@
+/*
+Components Used:
+- Arduino Board
+- Sensor Shield
+- Ultrasonic Sensor (HC-SR04)
+- Breadboard
+- Jumper Wires
+*/
+
+#define TRIG_PIN 9  // Trigger pin of HC-SR04 (Connected to D9 on Sensor Shield)
+#define ECHO_PIN 10 // Echo pin of HC-SR04 (Connected to D10 on Sensor Shield)
+
+void setup() {
+  Serial.begin(9600);    // Start serial communication
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  long duration;
+  float distance;
+
+  // Send a 10µs pulse to trigger the sensor
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Read the echo pin
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Convert time to distance (Speed of sound = 343 m/s)
+  distance = (duration * 0.0343) / 2; // Distance in cm
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500); // Update every 500ms
+}
