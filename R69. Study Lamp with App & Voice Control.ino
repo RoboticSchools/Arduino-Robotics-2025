e0abfc0eab2b83a803f69f63d2c6c7251eca62fb
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 22:39:22 +0530

    Update R69. Study Lamp with App & Voice Control.ino

--- R69. Study Lamp with App & Voice Control.ino
+++ R69. Study Lamp with App & Voice Control.ino
@@ -1,1 +1,33 @@
 
+/*
+Components Used:
+- Arduino Board
+- Sensor Shield
+- Lamp
+- Bluetooth Module (HC-05 or HC-06)
+- Breadboard
+- Jumper Wires
+*/
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(2, 3); // RX, TX
+
+int ledPin = 9; // LED connected to PWM Pin 9
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set LED pin as output
+  BTSerial.begin(9600);    // Start Bluetooth communication
+}
+
+void loop() {
+  if (BTSerial.available()) { // If data is received from the app
+    int command = BTSerial.parseInt(); // Read the received integer
+
+    if (command == 1) {
+      analogWrite(ledPin, 255); // Turn LED ON (Full Brightness)
+    } 
+    else if (command == 0) {
+      analogWrite(ledPin, 0);  // Turn LED OFF
+    } 
+  }
+}
