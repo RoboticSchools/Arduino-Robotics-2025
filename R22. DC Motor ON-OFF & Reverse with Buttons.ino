8311c1ff0c2bbee551e163d936a05f2ee0242c90
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 19:44:06 +0530

    Create R22. DC Motor ON-OFF & Reverse with Buttons.ino

---
+++ R22. DC Motor ON-OFF & Reverse with Buttons.ino
@@ -0,0 +1,37 @@
+/*
+Components Used:
+- Arduino Board
+- Sensor Shield
+- Adafruit Motor Shield
+- DC Motor
+- 3 Push Buttons (ON, OFF, REVERSE)
+- Breadboard
+- Jumper Wires
+*/
+
+#include <AFMotor.h>
+
+AF_DCMotor motor(1);  // Motor connected to M1 on the shield
+
+int buttonOn = A0;      // ON button pin
+int buttonOff = A1;     // OFF button pin
+int buttonRev = A2;     // REVERSE button pin
+
+void setup() {
+  pinMode(buttonOn, INPUT_PULLUP);
+  pinMode(buttonOff, INPUT_PULLUP);
+  pinMode(buttonRev, INPUT_PULLUP);
+  motor.setSpeed(255);  // Set maximum speed (0-255)
+}
+
+void loop() {
+  if (digitalRead(buttonOn) == LOW) {
+    motor.run(FORWARD);  // Run motor forward
+  } 
+  else if (digitalRead(buttonOff) == LOW) {
+    motor.run(RELEASE);  // Stop motor
+  } 
+  else if (digitalRead(buttonRev) == LOW) {
+    motor.run(BACKWARD);  // Run motor in reverse
+  }
+}
