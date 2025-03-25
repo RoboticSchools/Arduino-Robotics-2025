715a3e8a399dd1e88f1cc4334fb2818104bd4282
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-25 17:23:39 +0530

    9. RGB LED Color Switcher.ino

--- 9. RGB LED Color Switcher.ino
+++ 9. RGB LED Color Switcher.ino
@@ -8,10 +8,10 @@ Components Used:
 - Jumper Wires
 */
 
-const int buttonPin = 7;  // Button connected to Pin 7
-const int redPin = 4;     // Red LED pin
-const int greenPin = 5;   // Green LED pin
-const int bluePin = 6;    // Blue LED pin
+int buttonPin = 7;  // Button connected to Pin 7
+int redPin = 4;     // Red LED pin
+int greenPin = 5;   // Green LED pin
+int bluePin = 6;    // Blue LED pin
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP); // Set button as input with internal pull-up resistor
