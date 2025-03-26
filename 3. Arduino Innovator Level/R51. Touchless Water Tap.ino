4ac88ffc717e15ef3c394c97e84d11adc1b76cb8
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-26 17:27:28 +0530

    Update R51. Touchless Water Tap.ino

--- 3. Arduino Innovator Level/R51. Touchless Water Tap.ino
+++ 3. Arduino Innovator Level/R51. Touchless Water Tap.ino
@@ -14,7 +14,6 @@ Components Used:
 AF_DCMotor waterPump(1);  // Motor connected to port 1 of the motor shield
 
 int irSensorPin = 3;  // IR Sensor connected to pin 3
-int irSensorValue = 0;  // Variable to store IR sensor reading
 
 void setup() {
   waterPump.setSpeed(150);  // Set speed of the water pump
@@ -22,9 +21,9 @@ void setup() {
 }
 
 void loop() {
-  irSensorValue = digitalRead(irSensorPin);  // Read the value from IR sensor
+  int irSensorValue = digitalRead(irSensorPin);  // Read the value from IR sensor
 
-  if (irSensorValue == HIGH) {
+  if (irSensorValue == 1) {
     // If IR sensor detects an object, turn on the water pump for 3 seconds
     waterPump.run(FORWARD);  // Activate water pump
     delay(3000);  // Keep pump on for 3 seconds
