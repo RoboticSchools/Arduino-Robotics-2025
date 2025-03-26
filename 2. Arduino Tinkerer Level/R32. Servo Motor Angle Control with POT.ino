9f5ab80c91861605ff388391c404289bf20bb3bb
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-26 15:45:47 +0530

    Update R32. Servo Motor Angle Control with POT.ino

--- 2. Arduino Tinkerer Level/R32. Servo Motor Angle Control with POT.ino
+++ 2. Arduino Tinkerer Level/R32. Servo Motor Angle Control with POT.ino
@@ -12,15 +12,13 @@ Components Used:
 Servo myServo; // Create a Servo object
 int potPin = A0;  // Potentiometer connected to A0
 int servoPin = 9; // Servo connected to pin 9
-int potValue;     // Variable to store potentiometer value
-int angle;        // Variable to store servo angle
 
 void setup() {
   myServo.attach(servoPin); // Attach the servo to the sensor shield
 }
 
 void loop() {
-  potValue = analogRead(potPin); // Read potentiometer value (0-1023)
-  angle = map(potValue, 0, 1023, 0, 180); // Map value to servo angle (0-180)
+  int potValue = analogRead(potPin); // Read potentiometer value (0-1023)
+  int angle = map(potValue, 0, 1023, 0, 180); // Map value to servo angle (0-180)
   myServo.write(angle); // Move servo to the mapped angle
 }
