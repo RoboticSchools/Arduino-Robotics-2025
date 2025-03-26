efb4f77b77c699363c60921cfea87e9f301b206c
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-26 19:43:30 +0530

    Update R59. IR Remote-Controlled RGB LED.ino

--- 3. Arduino Innovator Level/R59. IR Remote-Controlled RGB LED.ino
+++ 3. Arduino Innovator Level/R59. IR Remote-Controlled RGB LED.ino
@@ -1,5 +1,5 @@
 /*
-Components used:
+Components Used:
 - Arduino Board
 - Sensor Shield
 - IR Receiver Module (Connected to Sensor Shield)
@@ -11,26 +11,26 @@ Components used:
 #include <IRremote.h>
 
 // Define IR receiver pin
-#define IR_RECEIVE_PIN 8  
+int irReceivePin = 8;  
 
 // Define RGB LED pins
-#define RED_PIN 6
-#define GREEN_PIN 5
-#define BLUE_PIN 3
+int redPin = 6;
+int greenPin = 5;
+int bluePin = 3;
 
 // Define IR remote button codes
-#define IR_BUTTON_RED 12       // Button for Red LED
-#define IR_BUTTON_GREEN 24     // Button for Green LED
-#define IR_BUTTON_BLUE 94      // Button for Blue LED
-#define IR_BUTTON_OFF 64       // Button to turn off all LEDs
+int irButtonRed = 12;       // Button for Red LED
+int irButtonGreen = 24;     // Button for Green LED
+int irButtonBlue = 94;      // Button for Blue LED
+int irButtonOff = 64;       // Button to turn off all LEDs
 
 void setup() {
   Serial.begin(9600);  // Initialize serial communication
-  pinMode(RED_PIN, OUTPUT);   // Set Red LED pin as output
-  pinMode(GREEN_PIN, OUTPUT); // Set Green LED pin as output
-  pinMode(BLUE_PIN, OUTPUT);  // Set Blue LED pin as output
+  pinMode(redPin, OUTPUT);   // Set Red LED pin as output
+  pinMode(greenPin, OUTPUT); // Set Green LED pin as output
+  pinMode(bluePin, OUTPUT);  // Set Blue LED pin as output
 
-  IrReceiver.begin(IR_RECEIVE_PIN); // Start IR receiver
+  IrReceiver.begin(irReceivePin); // Start IR receiver
 }
 
 void loop() {
@@ -41,29 +41,29 @@ void loop() {
     IrReceiver.resume();  // Prepare IR receiver for next signal
 
     // Check which button was pressed and control the RGB LED accordingly
-    if (command == IR_BUTTON_RED) {
+    if (command == irButtonRed) {
       Serial.println("Red LED ON");
-      digitalWrite(RED_PIN, HIGH);   // Turn ON Red LED
-      digitalWrite(GREEN_PIN, LOW);  // Turn OFF Green LED
-      digitalWrite(BLUE_PIN, LOW);   // Turn OFF Blue LED
+      digitalWrite(redPin, HIGH);   // Turn ON Red LED
+      digitalWrite(greenPin, LOW);  // Turn OFF Green LED
+      digitalWrite(bluePin, LOW);   // Turn OFF Blue LED
     } 
-    else if (command == IR_BUTTON_GREEN) {
+    else if (command == irButtonGreen) {
       Serial.println("Green LED ON");
-      digitalWrite(RED_PIN, LOW);   // Turn OFF Red LED
-      digitalWrite(GREEN_PIN, HIGH); // Turn ON Green LED
-      digitalWrite(BLUE_PIN, LOW);  // Turn OFF Blue LED
+      digitalWrite(redPin, LOW);   // Turn OFF Red LED
+      digitalWrite(greenPin, HIGH); // Turn ON Green LED
+      digitalWrite(bluePin, LOW);  // Turn OFF Blue LED
     } 
-    else if (command == IR_BUTTON_BLUE) {
+    else if (command == irButtonBlue) {
       Serial.println("Blue LED ON");
-      digitalWrite(RED_PIN, LOW);   // Turn OFF Red LED
-      digitalWrite(GREEN_PIN, LOW);  // Turn OFF Green LED
-      digitalWrite(BLUE_PIN, HIGH); // Turn ON Blue LED
+      digitalWrite(redPin, LOW);   // Turn OFF Red LED
+      digitalWrite(greenPin, LOW);  // Turn OFF Green LED
+      digitalWrite(bluePin, HIGH); // Turn ON Blue LED
     } 
-    else if (command == IR_BUTTON_OFF) {
+    else if (command == irButtonOff) {
       Serial.println("RGB LED OFF");
-      digitalWrite(RED_PIN, LOW);   // Turn OFF Red LED
-      digitalWrite(GREEN_PIN, LOW);  // Turn OFF Green LED
-      digitalWrite(BLUE_PIN, LOW);  // Turn OFF Blue LED
+      digitalWrite(redPin, LOW);   // Turn OFF Red LED
+      digitalWrite(greenPin, LOW);  // Turn OFF Green LED
+      digitalWrite(bluePin, LOW);  // Turn OFF Blue LED
     } 
     else {
       Serial.println("Button not recognized"); // Handle unknown button presses
