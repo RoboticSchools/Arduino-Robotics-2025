610a6ef4128d6827bc73720f4e6f36487aee2cad
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-28 19:15:38 +0530

    Update 7. Dual Button LED Control.ino

--- 1. Arduino Learner Level/7. Dual Button LED Control.ino
+++ 1. Arduino Learner Level/7. Dual Button LED Control.ino
@@ -16,13 +16,19 @@ void setup() {
   pinMode(buttonOnPin, INPUT_PULLUP);  // Set Button 1 as input with internal pull-up resistor
   pinMode(buttonOffPin, INPUT_PULLUP); // Set Button 2 as input with internal pull-up resistor
   pinMode(ledPin, OUTPUT);             // Set LED pin as output
+  Serial.begin(9600);                  // Initialize Serial Monitor at 9600 baud
 }
 
 void loop() {
-  if (digitalRead(buttonOnPin) == 0) {  // If Button 1 is pressed
-    digitalWrite(ledPin, HIGH);           // Turn on LED
+  if (digitalRead(buttonOnPin) == LOW) {  // If Button 1 is pressed
+    digitalWrite(ledPin, HIGH);            // Turn on LED
+    Serial.println("Button ON Pressed - LED ON");
+    delay(200);  // Debounce delay
   }
-  if (digitalRead(buttonOffPin) == 0) { // If Button 2 is pressed
-    digitalWrite(ledPin, LOW);            // Turn off LED
+  
+  if (digitalRead(buttonOffPin) == LOW) { // If Button 2 is pressed
+    digitalWrite(ledPin, LOW);             // Turn off LED
+    Serial.println("Button OFF Pressed - LED OFF");
+    delay(200);  // Debounce delay
   }
 }
