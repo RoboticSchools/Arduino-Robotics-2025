468630b8f20e6f45a28affebba9758a0160ec8d6
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2025-03-28 19:53:07 +0530

    picto

--- 1. Arduino Learner Level/R13. QR Code Activated LED.ino
+++ 1. Arduino Learner Level/R13. QR Code Activated LED.ino
@@ -16,20 +16,27 @@ int ledPin = 9; // LED connected to PWM Pin 9
 void setup() {
   pinMode(ledPin, OUTPUT); // Set LED pin as output
   BTSerial.begin(9600);    // Start Bluetooth communication
+  Serial.begin(9600);      // Start Serial Monitor
 }
 
 void loop() {
   if (BTSerial.available()) { // If data is received from the app
     int command = BTSerial.read(); // Read the received integer
+    Serial.print("Received Command: ");
+    Serial.println(command);
 
     if (command == 1) {
       digitalWrite(ledPin, HIGH); // Turn LED ON (Full Brightness)
+      Serial.println("LED ON");
     } 
     else if (command == 0) {
       digitalWrite(ledPin, LOW);  // Turn LED OFF
+      Serial.println("LED OFF");
     } 
     else if (command >= 2 && command <= 255) { 
       analogWrite(ledPin, command); // Adjust LED brightness (2-255)
+      Serial.print("LED Brightness Set To: ");
+      Serial.println(command);
     }
   }
 }
--- 1. Arduino Learner Level/R15. Voice & QR Code controlled RGB LED.ino
+++ 1. Arduino Learner Level/R15. Voice & QR Code controlled RGB LED.ino
@@ -20,59 +20,70 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
   BTSerial.begin(9600); // Start Bluetooth communication
+  Serial.begin(9600);   // Start Serial Monitor
 }
 
 void loop() {
   if (BTSerial.available()) { // If data is received from the app
     int command = BTSerial.read(); // Read the received integer
+    Serial.print("Received Command: ");
+    Serial.println(command);
 
     // Red Color
     if (command == 1) {
       digitalWrite(redPin, HIGH);
       digitalWrite(greenPin, LOW);
       digitalWrite(bluePin, LOW);
+      Serial.println("Red ON");
     }
     // Green Color
     else if (command == 2) {
       digitalWrite(redPin, LOW);
       digitalWrite(greenPin, HIGH);
       digitalWrite(bluePin, LOW);
+      Serial.println("Green ON");
     }
     // Blue Color
     else if (command == 3) {
       digitalWrite(redPin, LOW);
       digitalWrite(greenPin, LOW);
       digitalWrite(bluePin, HIGH);
+      Serial.println("Blue ON");
     }
     // Yellow Color (Red + Green)
     else if (command == 4) {
       digitalWrite(redPin, HIGH);
       digitalWrite(greenPin, HIGH);
       digitalWrite(bluePin, LOW);
+      Serial.println("Yellow ON");
     }
     // Cyan Color (Green + Blue)
     else if (command == 5) {
       digitalWrite(redPin, LOW);
       digitalWrite(greenPin, HIGH);
       digitalWrite(bluePin, HIGH);
+      Serial.println("Cyan ON");
     }
     // Magenta Color (Red + Blue)
     else if (command == 6) {
       digitalWrite(redPin, HIGH);
       digitalWrite(greenPin, LOW);
       digitalWrite(bluePin, HIGH);
+      Serial.println("Magenta ON");
     }
     // White Color (All ON)
     else if (command == 7) {
       digitalWrite(redPin, HIGH);
       digitalWrite(greenPin, HIGH);
       digitalWrite(bluePin, HIGH);
+      Serial.println("White ON");
     }
     // OFF (All OFF)
     else if (command == 0) {
       digitalWrite(redPin, LOW);
       digitalWrite(greenPin, LOW);
       digitalWrite(bluePin, LOW);
+      Serial.println("RGB LED OFF");
     }
   }
 }
